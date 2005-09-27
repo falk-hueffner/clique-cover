b00bba791e8c222a4640b86e080d68e617e5783c
Falk Hueffner <hueffner@minet.uni-jena.de>
2005-09-27 10:32:59 +0000

    Exhaustively reduce whenever possible.

--- ECC.ml
+++ ECC.ml
@@ -14,6 +14,60 @@ let all_covered ecc = PSQueue.is_empty ecc.cache;;
 
 let identity x = x;;
 
+let (@@) f g = fun x -> f (g x);;
+
+let do_cover ecc clique =
+  let uncovered = Graph.clear_subgraph ecc.uncovered clique in
+  let cache =
+    IntSet.fold
+      (fun cache i ->
+	 IntSet.fold
+	   (fun cache j ->	      
+	      if i < j
+	      then PSQueue.remove cache (i, j)
+	      else cache)
+	   clique
+	   cache)
+      clique
+      ecc.cache in
+  let ecc =
+    { ecc with uncovered = uncovered; k = ecc.k + 1; cache = cache }
+  in
+    ecc, (fun cliques -> clique :: cliques)
+;;
+
+(* Reduce vertices adjacent to no uncovered edge. Restrict search to
+   VERTICES. *)
+let reduce_deg0vertices ecc vertices =
+  let g, uncovered =
+    IntSet.fold
+      (fun (g, uncovered) i ->
+	 if Graph.is_deg0 uncovered i
+	 then Graph.delete_vertex g i, Graph.delete_vertex uncovered i
+	 else g, uncovered)
+      vertices
+      (ecc.g, ecc.uncovered) in
+    (* FIXME update cache *)
+    { ecc with g = g; uncovered = uncovered }
+;;
+
+let reduce_only1maxcliq ecc =
+  let rec loop ecc restorer =
+    if PSQueue.is_empty ecc.cache
+    then ecc, restorer
+    else
+      let (i, j), (neighbors, num_neigbors), score = PSQueue.top ecc.cache in
+	if score > 0
+	then ecc, restorer
+	else
+	  let clique = IntSet.add neighbors i in
+	  let clique = IntSet.add clique j in
+	  let ecc, restorer' = do_cover ecc clique in
+	    loop ecc (restorer @@ restorer')
+  in
+    loop ecc identity
+;;
+
 let make g =
   if !Util.verbose then Printf.eprintf "heating up cache...%!";
   let cache =
@@ -36,39 +90,21 @@ let make g =
       g         = g;
       uncovered = g;
       k         = 0;
-      cache     = cache;
-    } in
-      ecc, identity
+      cache     = cache; } in
+    let ecc, restorer = reduce_only1maxcliq ecc in
+    let ecc = reduce_deg0vertices ecc (Graph.vertices ecc.g) in
+(*       Printf.printf "reduced to k = %d\n" ecc.k; *)
+      ecc, restorer
 ;;
 
 let branching_edge ecc =
   let (i, j), _, score = PSQueue.top ecc.cache in
-(*     Printf.eprintf "selecting edge %d %d with score %d\n%!" i j score; *)
+    (*     Printf.eprintf "selecting edge %d %d with score %d\n%!" i j score; *)
     (i, j)
 ;;
 
 let cover ecc clique =
-  let uncovered = Graph.clear_subgraph ecc.uncovered clique in
-  let cache =
-    IntSet.fold
-      (fun cache i ->
-	 IntSet.fold
-	   (fun cache j ->	      
-	      if i < j
-	      then PSQueue.remove cache (i, j)
-	      else cache)
-	   clique
-	   cache)
-      clique
-      ecc.cache in
-  let g =
-    IntSet.fold
-      (fun g i ->
-	 if Graph.is_deg0 ecc.uncovered i
-	 then Graph.delete_vertex g i
-	 else g)
-      clique
-      ecc.g in    
-  let ecc = { g = g; uncovered = uncovered; k = ecc.k + 1; cache = cache } in
-    ecc, (fun cliques -> clique :: cliques)
+  let ecc, restorer = do_cover ecc clique in
+  let ecc = reduce_deg0vertices ecc clique in
+    ecc, restorer
 ;;
